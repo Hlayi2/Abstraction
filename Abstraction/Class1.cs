3eb3db4b60e5e69b28ae01bc374740a212a00c24
Hlayi2 <katlegokatenguyaza01@gmail.com>
2025-02-28 12:12:03 +0200

    Add project files.

---
+++ Abstraction/Class1.cs
@@ -0,0 +1,31 @@
+﻿using Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    abstract class Class1
+    {
+        public abstract void AnimalSounds();
+
+        public void Sleep()
+        {
+            Console.WriteLine("zzzz");
+        }
+
+    }
+
+    class pig : Class1
+    {
+        public override void AnimalSounds()
+        {
+            Console.WriteLine("The pig's sound is");
+            Console.WriteLine("oink oink");
+        }
+    }
+}
+
+  
